0b951d818361ae8829cb7dc86853ed6f0be5ecee
mike <viriyananda.michael@gmail.com>
2017-02-20 13:09:56 +0700

    Adding new module analytic_distribution_mass_assign

---
+++ analytic_distribution_mass_assign/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Mass Changing Journal Item Analytic",
+    "version": "8.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "depends": [
+        "account_analytic_plans",
+        "analytic_account_mass_assign",
+    ],
+    "data": [
+        "views/account_distribution_mass_assign_view.xml"
+    ],
+}
