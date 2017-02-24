ba601a9974b56d37eff1d01d65fdbfc48da744e2
Andhitia Rama <andhitia.r@gmail.com>
2017-02-24 13:09:43 +0700

    Update __openerp__.py

--- analytic_account_mass_assign/__openerp__.py
+++ analytic_account_mass_assign/__openerp__.py
@@ -2,8 +2,8 @@
 # Copyright 2017 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "Mass Changing Journal Item Analytic",
-    "version": "8.0.1.0.0",
+    "name": "Mass Changing Journal Item Analytic Account",
+    "version": "8.0.1.0.1",
     "category": "Accounting",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
