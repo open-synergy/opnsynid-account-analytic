54e3cde1df6117004362b37f7103f6e538412514
mike <viriyananda.michael@gmail.com>
2018-02-09 14:53:56 +0700

    Adding New Module account_analytic_plan_active

---
+++ account_analytic_plan_active/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Account Analytic Plan Active",
+    "version": "8.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "depends": [
+        "account_analytic_plans",
+    ],
+    "data": [
+        "views/account_analytic_plan_instance_view.xml"
+    ],
+}
