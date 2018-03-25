46514f11f918df25da4bd0d913d6979d1925a730
Andhitia Rama <andhitia.r@gmail.com>
2018-03-25 22:05:34 +0700

    [8.0.1.0.0] stock_analytic_default

---
+++ stock_analytic_default/models/stock_move.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    @api.model
+    def _default_analytic_account_id(self):
+        analytic_default = self._get_analytic_default(
+            user_id=self.env.user.id)
+        return analytic_default and analytic_default.analytic_id
+
+    analytic_account_id = fields.Many2one(
+        default=lambda self: self._default_analytic_account_id(),
+    )
+
+    @api.model
+    def _get_analytic_default(
+            self, product_id=False, partner_id=False,
+            user_id=False, date=False,
+            company_id=False):
+        obj_default = self.env["account.analytic.default"]
+        analytic_default = obj_default.account_get(
+            product_id=product_id, partner_id=partner_id,
+            user_id=user_id, date=date, company_id=company_id)
+        return analytic_default
