503d2b5187b149a5a62fa067a4f016f1918e611c
mikevhe18 <viriyananda.michael@gmail.com>
2019-05-27 16:40:03 +0700

    [8.0.1.0.0][analytic_account_display_name]

---
+++ analytic_account_display_name/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Account Analytic Display Name",
+    "version": "8.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "depends": [
+        "analytic",
+    ],
+    "data": [
+    ],
+}
