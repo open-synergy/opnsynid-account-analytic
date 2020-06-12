a46c5a09479452b908e9cd87e99499b7291f05d6
azmimr67@gmail.com <azmimr67@gmail.com>
2020-06-12 20:33:56 +0700

    [8.0.1.0.0][account_analytic_line_analysis] Account Analytic Line Analysis Reporting

---
+++ account_analytic_line_analysis/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Account Analytic Line Analysis",
+    "version": "8.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "depends": [
+        "account_accountant",
+    ],
+    "data": [
+        "reports/account_analytic_line_analysis.xml",
+    ],
+}
