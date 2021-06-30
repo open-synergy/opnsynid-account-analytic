de2e757da959797c582a0a18370f49d9d597290a
mikevhe18 <viriyananda.michael@gmail.com>
2021-06-30 16:47:28 +0700

    [account_analytic_line_no_close_account] 8.0.1.0.0

---
+++ account_analytic_line_no_close_account/__openerp__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Analytic Line No Close Account",
+    "version": "8.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "depends": [
+        "analytic",
+    ],
+    "data": [],
+}
